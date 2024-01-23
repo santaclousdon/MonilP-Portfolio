6d4d843f2d4901255238a594a8520ec309493883
santaclous112 <santaclous112@gmail.com>
2024-01-23 04:43:47 -0500

    feat/ecommerce

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,9 @@ import styles from './Footer.module.css';
 export const Footer = ({ className }) => (
   <footer className={classes(styles.footer, className)}>
     <Text size="s" align="center">
-      <span className={styles.date}>{`© ${new Date().getFullYear()} Monil Patel.`}</span>
-      Crafted by yours truly
+      <span
+        className={styles.date}
+      >{`Copyright © ${new Date().getFullYear()} Monil Patel.`}</span>
     </Text>
   </footer>
 );
